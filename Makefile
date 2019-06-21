85457ff332fd86df1b1fab6b0ace90d3fae3e8a0
Nuno Fernandes <nuno.fernandes@gmail.com>
2019-06-21 12:18:47 +0100

    First commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+compile:
+	@g++ -o hello -static hello.cc
---
+++ hello.cc
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout << "Hello! This message is coming from a container \n ";
+return 0;
+
+}
